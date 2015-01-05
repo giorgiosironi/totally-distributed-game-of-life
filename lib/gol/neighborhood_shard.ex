a199def6e8ea795dc9c2de66c4c8546ddbe0f34e
Giorgio Sironi <info@giorgiosironi.com>
2015-01-05 15:25:41 +0100

    NeighborhoodShard now deduplicates possible neighborhoods and uses them to calculate one value for each :cell

--- lib/gol/neighborhood_shard.ex
+++ lib/gol/neighborhood_shard.ex
@@ -1,6 +1,6 @@
 defmodule GOL.NeighborhoodShard do
   use GenServer
-  alias GOL.ShardIndex
+  alias GOL.Neighborhood
 
   def start_link(neighborhood_events, generation, shard_index, opts \\ []) do
     GenServer.start_link(
@@ -10,7 +10,8 @@ defmodule GOL.NeighborhoodShard do
         generation: generation,
         shard_index: shard_index,
         totals_neighborhoods: [],
-        neighborhoods: []
+        total_neighborhoods_needed_received: 0,
+        neighborhoods: %{}
       },
       opts
     )
@@ -20,23 +21,46 @@ defmodule GOL.NeighborhoodShard do
     GenServer.call(shard, {:number_will_be, number})
   end
 
-  def needed_in(shard, position) do
-    GenServer.call(shard, {:needed_in, position})
+  def needed_in(shard, position, source) do
+    GenServer.call(shard, {:needed_in, position, source})
   end
 
   def handle_call({:number_will_be, number}, _from, state) do
-    {:reply, nil, Map.update!(state, :totals_neighborhoods, fn totals ->
+    state = Map.update!(state, :totals_neighborhoods, fn totals ->
       totals ++ [number]
-    end)}
-  end
-
-  def handle_call({:needed_in, position}, _from, state) do
-    {:reply, nil, Map.update!(state, :neighborhoods, fn neighborhoods ->
-      neighborhoods ++ [position]
-      #n = Neighborhood.start_link position
-      #n.one_neighbor_is_alive
-      #neighborhoods ++ [n]
-    end)}
+    end)
+    check_completion(state)
     {:reply, nil, state}
   end
+
+  def handle_call({:needed_in, position, source}, _from, state) do
+    state = Map.update!(state, :neighborhoods, fn neighborhoods ->
+      if Map.has_key?(neighborhoods, position) do
+        neighborhood = Map.get(neighborhoods, position)
+      else
+        {:ok, neighborhood} = Neighborhood.start_link position
+        neighborhoods = Map.put(neighborhoods, position, neighborhood)
+      end
+      Neighborhood.one_cell_is_alive neighborhood, source
+      neighborhoods
+    end)
+    state = Map.update!(state, :total_neighborhoods_needed_received, fn total -> total + 1 end)
+    check_completion(state)
+    {:reply, nil, state}
+  end
+
+  defp check_completion(state) do
+    if state.shard_index.total == Enum.count(state.totals_neighborhoods)
+    && Enum.reduce(state.totals_neighborhoods, fn a, b -> a + b end) == state.total_neighborhoods_needed_received do
+      # TODO: total_neighborhoods_needed_received is incorrect, we should use the number of neighborhoods. Add a relevant test
+      GenEvent.sync_notify state.neighborhood_events, {:cells_considered, state.shard_index, Enum.count(state.neighborhoods)}
+      Map.values(state.neighborhoods) |>
+      Enum.each(fn neighborhood ->
+        Neighborhood.to_new_cell neighborhood, fn center, life -> 
+          GenEvent.sync_notify state.neighborhood_events, {:cell, state.shard_index, center, life} 
+        end
+      end)
+    end
+  end
+
 end
--- test/gol/neighborhood_shard_test.exs
+++ test/gol/neighborhood_shard_test.exs
@@ -24,13 +24,11 @@ defmodule GOL.NeighborhoodShardTest do
     NeighborhoodShard.number_will_be shard, 0
     NeighborhoodShard.number_will_be shard, 0
     NeighborhoodShard.number_will_be shard, 1
-    NeighborhoodShard.needed_in shard, Position.xy(0, 0)
-    NeighborhoodShard.needed_in shard, Position.xy(0, 1)
-    NeighborhoodShard.needed_in shard, Position.xy(0, 2)
+    NeighborhoodShard.needed_in shard, Position.xy(0, 1), Position.xy(0, 0)
+    NeighborhoodShard.needed_in shard, Position.xy(0, 1), Position.xy(0, 1)
+    NeighborhoodShard.needed_in shard, Position.xy(0, 1), Position.xy(0, 2)
 
-    assert_receive {:cells_considered, own_shard, 2}
-    solitude_center = Position.xy 0, 0
-    assert_receive {:cell, own_shard, solitude_center, :dead}
+    assert_receive {:cells_considered, own_shard, 1}
     populated_center = Position.xy 0, 1
     assert_receive {:cell, own_shard, populated_center, :alive}
 
