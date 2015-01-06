25aa651ec34d6a17a8f43e430eb81152340c209d
Giorgio Sironi <info@giorgiosironi.com>
2015-01-06 11:35:03 +0100

    Added Facade for population and interrogation of CellShards in a single shot

---
+++ lib/gol/facade.ex
@@ -0,0 +1,29 @@
+defmodule GOL.Facade do
+  alias GOL.ShardIndex
+  alias GOL.CellShard
+
+  def empty_generation total_shards do
+    for i <- 0..total_shards-1, into: %{} do
+      {:ok, manager} = GenEvent.start_link
+      shard_index = ShardIndex.from "#{i}in4"
+      {:ok, shard} = CellShard.start_link manager, 1, shard_index
+      {shard_index, shard}
+    end
+  end
+
+  def populate_alive_cell(generation, position) do
+    # TODO: duplication of position.x
+    index = Map.keys(generation) |>
+            Enum.find(fn shard_index ->
+              ShardIndex.contains(shard_index, position.x)
+            end)
+    CellShard.populate_alive_cell Map.get(generation, index), position
+  end
+
+  def alive(generation) do
+    Map.values(generation) |>
+    Enum.flat_map(fn shard ->
+      CellShard.alive shard
+    end)
+  end
+end
---
+++ test/gol/facade_test.exs
@@ -0,0 +1,12 @@
+defmodule GOL.FacadeTest do
+  use ExUnit.Case
+  alias GOL.Facade
+  alias GOL.Position
+
+  test "populates and retrieves cells from multiple shards" do
+    generation = Facade.empty_generation 4
+    Facade.populate_alive_cell generation, Position.xy(1, 0)
+    Facade.populate_alive_cell generation, Position.xy(3, 4)
+    assert [Position.xy(1, 0), Position.xy(3, 4)] == Facade.alive generation
+  end
+end
