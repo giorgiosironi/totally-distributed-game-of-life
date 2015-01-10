cb9e4a9878f0681a1de4236b6978289bc903bb2f
Giorgio Sironi <info@giorgiosironi.com>
2015-01-10 16:18:17 +0100

    Removed shard creation duplication in test

--- test/gol/cell_shard_test.exs
+++ test/gol/cell_shard_test.exs
@@ -8,34 +8,30 @@ defmodule GOL.CellShardTest do
   setup do
     {:ok, manager} = GenEvent.start_link
     :ok = GenEvent.add_mon_handler(manager, Forwarder, self())
-    # TODO: extract here creation of own_shard_index and shard
+    own_shard_index = ShardIndex.from "0in4"
+    {:ok, shard} = CellShard.start_link manager, 1, ShardIndex.from "0in4"
 
-    {:ok, manager: manager}
+    {:ok, own_shard_index: own_shard_index, shard: shard}
   end
 
-  test "knows a set of cells and their positions", %{manager: manager} do
-    {:ok, shard} = CellShard.start_link manager, 1, ShardIndex.from "0in4"
+  test "knows a set of cells and their positions", %{shard: shard} do
     CellShard.populate_alive_cell shard, Position.xy(0, 6)
     assert [Position.xy(0, 6)] == CellShard.alive shard
   end
 
-  test "can test the lifeness of a cell", %{manager: manager} do
-    {:ok, shard} = CellShard.start_link manager, 1, ShardIndex.from "0in4"
+  test "can test the lifeness of a cell", %{shard: shard} do
     CellShard.populate_alive_cell shard, Position.xy(0, 6)
     assert true == CellShard.alive shard, Position.xy(0, 6)
     assert false == CellShard.alive shard, Position.xy(1, 6)
   end
 
-  test "adding alive cells is idempotent", %{manager: manager} do
-    {:ok, shard} = CellShard.start_link manager, 1, ShardIndex.from "0in4"
+  test "adding alive cells is idempotent", %{shard: shard} do
     CellShard.populate_alive_cell shard, Position.xy(0, 6)
     CellShard.populate_alive_cell shard, Position.xy(0, 6)
     assert 1 == Enum.count CellShard.alive shard
   end
 
-  test "tells all cells to evolve", %{manager: manager} do
-    own_shard_index = ShardIndex.from "0in4"
-    {:ok, shard} = CellShard.start_link manager, 1, own_shard_index
+  test "tells all cells to evolve", %{own_shard_index: own_shard_index, shard: shard} do
     CellShard.populate_alive_cell shard, Position.xy(0, 6)
     CellShard.evolve shard
     assert_receive {:neighborhood_needed_number, ^own_shard_index, 3}
@@ -52,16 +48,12 @@ defmodule GOL.CellShardTest do
     assert_receive {:neighborhood_needed, ^near_shard_index, ^near_shard_cell_position, ^existing_cell_position}
   end
 
-  test "some cell shards may be empty", %{manager: manager} do
-    own_shard_index = ShardIndex.from "0in4"
-    {:ok, shard} = CellShard.start_link manager, 1, own_shard_index
+  test "some cell shards may be empty", %{own_shard_index: own_shard_index, shard: shard} do
     CellShard.evolve shard
     assert_receive {:neighborhood_needed_number, ^own_shard_index, 0}
   end
 
-  test "registers an evolution", %{manager: manager} do
-    own_shard_index = ShardIndex.from "0in4"
-    {:ok, shard} = CellShard.start_link manager, 1, own_shard_index
+  test "registers an evolution", %{shard: shard} do
     CellShard.register_evolved_cell shard, Position.xy(0, 2), :alive
     CellShard.register_evolved_cell shard, Position.xy(4, 5), :dead
     CellShard.register_evolved_number shard, 2
@@ -69,9 +61,7 @@ defmodule GOL.CellShardTest do
     assert [Position.xy(0, 2)] == CellShard.alive shard
   end
 
-  test "a partial evolution blocks the alive calls until complete registration", %{manager: manager} do
-    own_shard_index = ShardIndex.from "0in4"
-    {:ok, shard} = CellShard.start_link manager, 1, own_shard_index
+  test "a partial evolution blocks the alive calls until complete registration", %{shard: shard} do
     CellShard.register_evolved_cell shard, Position.xy(0, 0), :alive
 
     spawn_link(fn -> assert [Position.xy(0, 0), Position.xy(0, 1)] == CellShard.alive shard end)
@@ -83,9 +73,7 @@ defmodule GOL.CellShardTest do
     CellShard.register_evolved_number shard, 2
   end
 
-  test "when declared empty can be consulted with alive calls", %{manager: manager} do
-    own_shard_index = ShardIndex.from "0in4"
-    {:ok, shard} = CellShard.start_link manager, 1, own_shard_index
+  test "when declared empty can be consulted with alive calls", %{shard: shard} do
     CellShard.declare_empty shard
 
     assert [] == CellShard.alive shard
