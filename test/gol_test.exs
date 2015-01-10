40808d57633506a618bda5ce51b29800e5cb4ae4
Giorgio Sironi <info@giorgiosironi.com>
2015-01-10 15:33:08 +0100

    Adding failing test for 1 shard plane, seems there are hardcoding somewhere

--- test/gol_test.exs
+++ test/gol_test.exs
@@ -3,8 +3,34 @@ defmodule GOLTest do
   alias GOL.Position
   alias GOL.Facade
 
-  test "a bar rotates" do
+  test "a bar rotates, 1 shard" do
+    first_generation = Facade.empty_generation 1
+
+    bar_rotates first_generation
+  end
+
+  test "a bar rotates, 4 shards" do
     first_generation = Facade.empty_generation 4
+
+    bar_rotates first_generation
+  end
+
+  test "a block stays still, 4 shards" do
+
+    first_generation = Facade.empty_generation 4
+    Facade.populate_alive_cell first_generation, Position.xy(1, 1)
+    Facade.populate_alive_cell first_generation, Position.xy(1, 2)
+    Facade.populate_alive_cell first_generation, Position.xy(2, 1)
+    Facade.populate_alive_cell first_generation, Position.xy(2, 2)
+ 
+    second_generation = Facade.evolve first_generation
+    assert [Position.xy(1, 1), 
+            Position.xy(1, 2), 
+            Position.xy(2, 1), 
+            Position.xy(2, 2)] == Facade.alive second_generation
+  end
+
+  defp bar_rotates first_generation do
     Facade.populate_alive_cell first_generation, Position.xy(1, 0)
     Facade.populate_alive_cell first_generation, Position.xy(1, 1)
     Facade.populate_alive_cell first_generation, Position.xy(1, 2)
@@ -18,20 +44,5 @@ defmodule GOLTest do
     assert [Position.xy(1, 0), 
             Position.xy(1, 1), 
             Position.xy(1, 2)] == Facade.alive third_generation
-  end
-
-  test "a block stays still" do
-
-    first_generation = Facade.empty_generation 4
-    Facade.populate_alive_cell first_generation, Position.xy(1, 1)
-    Facade.populate_alive_cell first_generation, Position.xy(1, 2)
-    Facade.populate_alive_cell first_generation, Position.xy(2, 1)
-    Facade.populate_alive_cell first_generation, Position.xy(2, 2)
- 
-    second_generation = Facade.evolve first_generation
-    assert [Position.xy(1, 1), 
-            Position.xy(1, 2), 
-            Position.xy(2, 1), 
-            Position.xy(2, 2)] == Facade.alive second_generation
   end
 end
